aa8656236b558512fc355e6d595f0e31697eb510
mvcastro <mvcastro1975@gmail.com>
2024-10-10 17:18:29 -0300

    Atualização da classe SerieTemporal

--- balanco_hidrico_reservatorios/serie_temporal.py
+++ balanco_hidrico_reservatorios/serie_temporal.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Literal, TypedDict
 
 import pandas as pd
@@ -73,7 +73,7 @@ class ColunasSerieTemporal(TypedDict):
 
 @dataclass
 class SerieTemporal:
-    dataframe: pd.DataFrame
+    dataframe: pd.DataFrame = field(repr=False)
     nome_das_colunas: ColunasSerieTemporal
     freq: Frequencia
 
